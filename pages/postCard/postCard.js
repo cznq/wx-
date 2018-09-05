d5ac5aab1ce61a886c38fd9f53a0a42453a76b6b
zijun.wan <1518208233@qq.com>
2018-09-05 19:44:27 +0800

    fix bug

--- pages/postCard/postCard.js
+++ pages/postCard/postCard.js
@@ -9,69 +9,97 @@ Page({
     autoplay: false,
     interval: 5000,
     duration: 1000,
-    imgUrls: [
-      {url:'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',name:'父情节'},
-      {url:'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',name:'小墨兰'},
-      {url:'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',name:'附近的'},
-      {url:'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',name:'戴假发'},
-      {url:'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',name:'角度看'},
-      {url:'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',name:'觉得的'}
+    imgUrls: [{
+        url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        name: '父情节'
+      },
+      {
+        url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        name: '小墨兰'
+      },
+      {
+        url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+        name: '附近的'
+      },
+      {
+        url: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        name: '戴假发'
+      },
+      {
+        url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        name: '角度看'
+      },
+      {
+        url: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+        name: '觉得的'
+      }
     ],
+    platform: 'ios'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    var phone = wx.getSystemInfoSync();  //调用方法获取机型    
+    var that = this;
+    if (phone.platform == 'ios') {   
+      that.setData({    
+        platform: 'ios'           
+      });
+    } else if (phone.platform == 'android') {
+      that.setData({            
+        platform: 'android'           
+      });        
+    }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
